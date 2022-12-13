e867ddb8ff8c156168d8c22e3ab00d89f4abb913
Joao Vitor Mocambite Lima <86741407+JoaoVitor2022dev@users.noreply.github.com>
2022-12-13 01:20:18 -0400

    Update App.js

--- react-portifolio/src/App.js
+++ react-portifolio/src/App.js
@@ -1,9 +1,17 @@
-function App() {
-  return (
-    <div className="App">
+import Navbar from "../src/scenes/Navbar";
+import { useState } from 'react'; 
+import  useMediaQuery  from "../src/hooks/useMediaQuery";
 
+function App() {
+  const [selectedPage, setSelectedPage] = useState('home'); 
+   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");  
+  
+  return (
+    <div className="app bg-depp-blue">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
     </div>
   );
 }
 
 export default App;
+
