974333cae5973f587e647f2207c009266d9bfeae
Joao Vitor Mocambite Lima <86741407+JoaoVitor2022dev@users.noreply.github.com>
2022-12-14 00:22:35 -0400

    Update Navbar.jsx

--- react-portifolio/src/scenes/Navbar.jsx
+++ react-portifolio/src/scenes/Navbar.jsx
@@ -2,13 +2,44 @@ import { useState } from "react";
 import AnchorLink from "react-anchor-link-smooth-scroll"; 
 import useMediaQuery from "../hooks/useMediaQuery";
 
-const Navbar = ({ selectecPage , setSelectecPage }) => {
+const Link = ( {page , selectecPage , setSelectecPage }) => {
+   const lowerCasePage = page.toLowerCase(); 
+    return (
+      <AnchorLink
+      className={`${ selectecPage === lowerCasePage ?  "text-yellow" : ""} 
+         hover:text-yellow transition duration-500`}
+         href={`#${lowerCasePage}`}
+         onClick={() => selectecPage(lowerCasePage)}
+         >
+          {page}
+         </AnchorLink>
+    )
+}; 
+
+const Navbar = ({ selectedPage , setSelectecPage }) => {
   const [ isMenuToggled, setIsMenuToggled ] = useState(false);  
    const isAboveSmallScreens = useMediaQuery("(min-width: 768px)"); 
+
   return (
     <nav className={`z-40 w-full fixed top-0 py-6`}>
+       <div className="flex items-center  justify-between mx-auto w-5/6">
+            <h4 className="font-playfair text-3xl font-bold">JE</h4>      
+
+            {/* DESKTOP NAV */}
+            {isAboveSmallScreens ? ( 
+              <div className="flex justify-between gap-16 font-opensans text-sm font-semibold">
+                  <Link
+                  page="Home"
+                  selectedPage={selectedPage}
+                  setSelectecPage={setSelectecPage}
+                  />
+              </div>
+            ): ( <div></div>)}
+       </div>
     </nav>
   )
 }
 
-export default Navbar
+export default Navbar; 
+
+
