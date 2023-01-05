0c1d40e613397b1330b6dce1dd62960c4152e49a
Joao Vitor Mocambite Lima <86741407+JoaoVitor2022dev@users.noreply.github.com>
2023-01-05 16:36:08 -0400

    Add files via upload

---
+++ portifolio/react_sass/src/components/SideBar.jsx
@@ -0,0 +1,15 @@
+
+
+const SideBar = () => {
+  return (
+        <aside id="sidebar">
+          <p>Imagem</p>
+          <p className="title">Desevolvedor Full-stack</p>
+          <p>redes sociais</p>
+          <p>informaçoes de contato</p>
+          <a href="" className="btn">Download Curriculo</a> 
+        </aside>
+  )
+}
+
+export default SideBar;
